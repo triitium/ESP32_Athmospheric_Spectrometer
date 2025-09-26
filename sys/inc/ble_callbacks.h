cbce2c2d786b02ae96b6dc52585aa050afd442b6
Stefan <76401862+triitium@users.noreply.github.com>
2025-09-26 13:51:43 +0200

    Update ble_callbacks.h

--- sys/inc/ble_callbacks.h
+++ sys/inc/ble_callbacks.h
@@ -1,13 +1,7 @@
 #pragma once
+#include "main.h"
 #include "esp_gatts_api.h"
 #include "esp_gap_ble_api.h"
-#include <stdbool.h>
-#include <stdint.h>
-
-extern uint16_t char_handle;
-extern uint16_t conn_id;
-extern esp_gatt_if_t gatts_if;
-extern bool notify_enabled;
 
 void gatts_event_handler(esp_gatts_cb_event_t event,
                          esp_gatt_if_t gatts_if_param,
